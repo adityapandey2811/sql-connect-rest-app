2a23d714b10c74d124a3ad45f8d77448e6cfbf99
Aditya Pandey <2811adityapandey@gmail.com>
2023-06-27 15:18:00 +0530

    Add files via upload

---
+++ src/test/java/com/example/sqlconnectrestapp/SqlConnectRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlconnectrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlConnectRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
